e26a393b0a39b6f38443642d4878b75747aaeaf9
Dung Hoang <hoangdung.200298@gmail.com>
2025-05-21 16:27:17 +0700

    test: add new category

--- src/pages/catalog/category-list.tsx
+++ src/pages/catalog/category-list.tsx
@@ -5,9 +5,10 @@ import { useAtomValue } from "jotai";
 import { useNavigate } from "react-router-dom";
 import { categoriesState } from "@/state";
 import { useEffect, useState } from "react";
-import { Icon } from "zmp-ui";
+import { Button, Icon, Input } from "zmp-ui";
 
-const API_URL = "https://lukakuvic.vercel.app/api/native/categories";
+const API_URL = "http://127.0.0.1:3000/api/native/categories";
+// const API_URL = "https://lukakuvic.vercel.app/api/native/categories";
 type CategoryType = {
   id: string;
   name: string;
@@ -18,7 +19,31 @@ export default function CategoryListPage() {
 
   const categories = useAtomValue(categoriesState);
 
+  const [newCategoryName, setNewCategoryName] = useState<string>("");
+
   const token = import.meta.env.VITE_API_ACCESS_TOKEN || "";
+
+  const handleCreateNewCategory = () => {
+    console.log({ newCategory: newCategoryName });
+    fetch(API_URL, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ name: newCategoryName }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.payload) {
+          setNewCategories([...newCategories, data.payload]);
+          setNewCategoryName("");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   useEffect(() => {
     fetch(API_URL, {
@@ -65,13 +90,29 @@ export default function CategoryListPage() {
       <div className="grid grid-cols-4 p-4 gap-x-4 gap-y-8">
         {newCategories &&
           newCategories?.map((c) => (
-            <div className="flex flex-col items-center space-y-2 overflow-hidden cursor-pointer">
+            <div
+              key={c.id}
+              className="flex flex-col items-center space-y-2 overflow-hidden cursor-pointer"
+            >
               <Icon icon="zi-gallery" />
-              <p className="text-sm" key={c.id}>
-                {c.name}
-              </p>
+              <p className="text-sm">{c.name}</p>
             </div>
           ))}
+      </div>
+      <div className="p-4">
+        <Input
+          helperText="Please enter the name of the category."
+          label="Category name"
+          value={newCategoryName}
+          onChange={(e) => {
+            setNewCategoryName(e.target.value as string);
+          }}
+        />
+        <div className="flex flex-col items-center">
+          <Button onClick={handleCreateNewCategory} variant="primary">
+            Tạo
+          </Button>
+        </div>
       </div>
     </>
   );
